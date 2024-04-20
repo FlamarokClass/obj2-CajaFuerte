fa1ce448275156de4829475126814c12597522d0
FrancoAnderson <francoleonel.andersonrojas@estudiantes.unahur.edu.ar>
2024-04-19 21:22:45 -0300

    falta terminar

--- src/ar/edu/unahur/po2/cajaFuerte/CajaFuerte.java
+++ src/ar/edu/unahur/po2/cajaFuerte/CajaFuerte.java
@@ -3,9 +3,19 @@ package ar.edu.unahur.po2.cajaFuerte;
 public class CajaFuerte {
 	private Boolean abierta = true;
 	private Integer clave = 1234;
+	private Boolean bloqueada = false;
+	private Integer intentos = 0;
 	
 	public Boolean estaAbierta() {
 		return abierta;
+	}
+	
+	public Boolean estaBloqueada() {
+		return bloqueada;
+	}
+	
+	public Integer cantidadDeIntentos() {
+		return intentos;
 	}
 	
 	public void cerrar() {
@@ -19,10 +29,20 @@ public class CajaFuerte {
 	public void abrirConClave(Integer miClave) {
 		if(this.clave.equals(miClave)) {
 			this.abrir();
+			this.intentos = 0;
+		} else {
+			this.intentos += 1;
 		}
 	}
 	
-	public void configurarContraseña(Integer miClave) {
+	public void configurarContrasenia(Integer miClave) {
 		this.clave = miClave;
+	}
+	
+	public void bloquearCaja() {
+		if(this.abierta.equals(false) && this.intentos >= 3) {
+			this.cerrar();
+			this.bloqueada = true;
+		}
 	}
 }
--- test/ar/edu/unahur/po2/cajaFuerte/CajaFuerteTest.java
+++ test/ar/edu/unahur/po2/cajaFuerte/CajaFuerteTest.java
@@ -29,11 +29,33 @@ public class CajaFuerteTest {
 	}
 	
 	@Test 
-	public void quealAbrirConClaveSeAbra() {
+	public void queAlAbrirConClaveSeAbra() {
 		CajaFuerte caja = new CajaFuerte();
 		caja.cerrar();
 		caja.abrirConClave(1234);
 		assertEquals(true,caja.estaAbierta());
 	}
+	
+	@Test 
+	public void queAlAbrirConMiClaveSeAbra() {
+		CajaFuerte caja = new CajaFuerte();
+		caja.cerrar();
+		caja.configurarContrasenia(2312);
+		caja.abrirConClave(2312);
+		assertEquals(true,caja.estaAbierta());
+	}
+	
+	@Test
+	public void seBloqueaAlTercerIntentoErroneo() {
+		CajaFuerte caja = new CajaFuerte();
+		caja.cerrar();
+		caja.configurarContrasenia(2312);
+		caja.abrirConClave(2311);
+		caja.abrirConClave(2314);
+		caja.abrirConClave(2316);
+		assertEquals(true,caja.estaBloqueada());
+		
+	}
+	
 
 } 
